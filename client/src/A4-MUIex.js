33baeb7ceab931744862926e3886b8b8c0804479
ant-shen <anthony.shen11@gmail.com>
2023-10-02 16:28:47 -0700

    Create A4-MUIex.js

---
+++ client/src/A4-MUIex.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+function MyMuiComponent() {
+  return (
+    <div>
+      <Typography variant="h4">Material-UI Example</Typography>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
+    </div>
+  );
+}
+
+export default MyMuiComponent;
