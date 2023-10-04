af4016fe550a1b5cf86d3c9db62fd74e46210032
greatmimic <tjdgns1256@gmail.com>
2023-10-03 23:22:30 -0700

    updated login page. needs pictures of club interior and outside.

--- client/src/components/signUpForm.js
+++ client/src/components/signUpForm.js
@@ -106,7 +106,7 @@ export default function SignUpForm() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign up
+            Register for CPP Lifting Club
           </Typography>
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
@@ -177,7 +177,7 @@ export default function SignUpForm() {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/" variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
