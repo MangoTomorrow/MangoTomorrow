8e2d66382549d40f68d1d4dd092cdd91159c857b
Brian (Sung) Kang <tjdgns1256@gmail.com>
2023-10-18 15:35:53 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 433;
+const port = process.env.PORT || 443;
 
 app.get('/', (req, res) => {
     res.send('hello world');
