0852b9b03e79a1a22cba7399b01fba6b05a95aaf
greatmimic <tjdgns1256@gmail.com>
2023-10-18 17:11:00 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,12 +5,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.use(express.static('build'));
 
 app.get('/', (req, res) => {
-    res.sendFile('build/index.html', {root: __dirname});
-});
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
