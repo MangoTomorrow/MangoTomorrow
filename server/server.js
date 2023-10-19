7a279ae8c15d5e25fde138bc0e8466457dd9191e
greatmimic <tjdgns1256@gmail.com>
2023-10-18 17:06:46 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,8 +5,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
+
+app.use(express.static('build'));
+
 app.get('/', (req, res) => {
-    res.send('finally connected ');
+    res.sendFile('build/index.html', {root: __dirname});
 });
 
 app.listen(port, () => {
