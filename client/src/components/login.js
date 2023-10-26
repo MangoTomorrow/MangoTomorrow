578888b9bbea3e1bc982e1690704784bd8946fb4
greatmimic <tjdgns1256@gmail.com>
2023-10-26 08:08:48 -0700

    test cicd

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -23,7 +23,7 @@ function Copyright(props) {
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        MangoTomorrow
+        MangoTomorrow test hello world
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
