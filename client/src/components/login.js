ff4118674a68b735109b56a50f13dfd7e620f377
greatmimic <tjdgns1256@gmail.com>
2023-10-26 08:10:59 -0700

    reverted test

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -23,7 +23,7 @@ function Copyright(props) {
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        MangoTomorrow test hello world
+        MangoTomorrow 
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
