9754bf3e3f4e0863d4b70a84ddd645fb319fb021
greatmimic <tjdgns1256@gmail.com>
2023-12-03 08:05:20 -0800

    .

--- client/src/components/protectedRoute.js
+++ client/src/components/protectedRoute.js
@@ -5,9 +5,9 @@ import { useAuth } from './authContext';
 
 const ProtectedRoute = ({ children, allowedRoles }) => {
     const { isAuthenticated, userRole } = useAuth();
-        console.log('user role above protected route: ', userRole);
+        
     if(!isAuthenticated || (allowedRoles && !allowedRoles.includes(userRole))) {
-        console.log('user role at protected route', userRole);
+        
         return <Navigate to="/" />;
     }
     return children;
